fc6081d0aca93c43e4a238e05bc32a35933e60bc
Nick <90505328+napswastaken@users.noreply.github.com>
2023-08-17 15:19:05 +1200

    Commit

---
+++ TraderPlus_Sorter.py
@@ -0,0 +1,32 @@
+'''
+
+This Python script is designed to read and organize pricing information from the TraderPlusPriceConfig.json file,
+which is used in the DayZ Mod TraderPlus.
+Mod Link: https://steamcommunity.com/sharedfiles/filedetails/?id=2458896948
+The script arranges the pricing data in descending order, placing items with the highest buy price at the top.
+
+'''
+__author__ = "naps"
+__copyright__ = "Copyright (C) 2023 Nick Shepherd"
+__license__ = "General Public License v3.0"
+__version__ = "1.0"
+
+import os
+import json
+
+input_file = r"Path to your TraderPlusPriceConfig.json"
+output_file = os.path.join(os.path.dirname(input_file), "TraderPlusPriceConfig_sorted.json")
+
+with open(input_file, "r") as f:
+    data = json.load(f)
+
+def sort_by_buy_price(product):
+    return int(product.split(',')[-2])
+
+for category in data["TraderCategories"]:
+    category["Products"].sort(key=sort_by_buy_price, reverse=True)
+
+with open(output_file, "w") as f:
+    json.dump(data, f, indent=4)
+
+print(f"Sorted data written to {output_file}")
