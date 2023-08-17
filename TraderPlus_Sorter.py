8b5be42fcbda888285040c1430ecbeb77de9fbf5
Nick <90505328+napswastaken@users.noreply.github.com>
2023-08-17 15:29:24 +1200

    Added a few comments to make it easier to understand.

--- TraderPlus_Sorter.py
+++ TraderPlus_Sorter.py
@@ -14,14 +14,14 @@ __version__ = "1.0"
 import os
 import json
 
-input_file = r"Path to your TraderPlusPriceConfig.json"
-output_file = os.path.join(os.path.dirname(input_file), "TraderPlusPriceConfig_sorted.json")
+input_file = r"Path-to-your-TraderPlusPriceConfig" # Point towards your TraderPlusPriceConfig.json file.
+output_file = os.path.join(os.path.dirname(input_file), "TraderPlusPriceConfig_sorted.json") # Output's to the same location as your input.
 
 with open(input_file, "r") as f:
     data = json.load(f)
 
 def sort_by_buy_price(product):
-    return int(product.split(',')[-2])
+    return int(product.split(',')[-2]) # This should never change unless Dmitri changes it and screws us all.
 
 for category in data["TraderCategories"]:
     category["Products"].sort(key=sort_by_buy_price, reverse=True)
